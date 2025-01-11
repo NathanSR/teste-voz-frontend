aa00101187249550237258d09d6c50a13dc74235
Nathan Santos <nathansr.nsr@gmail.com>
2025-01-10 22:43:19 -0300

    add swipper slider to cars section

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,8 +1,8 @@
 import logo from '../public/images/logo.png'
-import bg1Img from '../public/images/bg1-without-car.jpg'
+import bg1Img from '../public/images/bg1.png'
 import bg2Img from '../public/images/bg2.jpg'
 import './App.css'
-import { Scene3D } from './components/Scene3D'
+
 import { AiOutlineFacebook, AiOutlineUser } from 'react-icons/ai'
 import { CiCalendarDate } from 'react-icons/ci'
 import { IoSpeedometerOutline } from 'react-icons/io5'
@@ -10,11 +10,18 @@ import { GoZap } from 'react-icons/go'
 import { FiPhone, FiUsers } from 'react-icons/fi'
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
 
 const cars = [
 	{ id: 1, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car1.png" },
 	{ id: 2, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car2.png" },
 	{ id: 3, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car3.png" },
+	{ id: 4, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car2.png" },
+	{ id: 5, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car1.png" },
+	{ id: 6, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car3.png" },
+	{ id: 7, name: 'Mustang', year: 2015, velocity: "180 Km/h", energy: "9/10", people: "8/10", img: "./images/car2.png" },
 ]
 
 function App() {
@@ -41,40 +48,53 @@ function App() {
 			<section id='initial'>
 				<img src={bg1Img} alt="car" />
 				<span>MUSTANG</span>
-				{/* <Scene3D /> */}
 				{/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#EBEFF2" fill-opacity="1" d="M0,96L120,128C240,160,480,224,720,229.3C960,235,1200,181,1320,154.7L1440,128L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg> */}
 			</section>
 
 			{/* Seção de lista de carros */}
 			<section id='cars'>
-				{cars.map(car => (
-					<div key={car.id} className='card-car animate-view'>
-						<img src={car.img} alt="car" />
-						<h1>{car.name}</h1>
-						<p>
-							<CiCalendarDate />
-							<span>{car.year}</span>
-						</p>
-						<p>
-							<IoSpeedometerOutline />
-							<span>{car.velocity}</span>
-						</p>
-						<p>
-							<GoZap />
-							<span>{car.energy}</span>
-						</p>
-						<p>
-							<FiUsers />
-							<span>{car.people}</span>
-						</p>
+				<Swiper
+					modules={[Navigation]}
+					navigation
+					spaceBetween={20}
+					slidesPerView={1}
+					breakpoints={{
+						640: { slidesPerView: 2 },
+						1200: { slidesPerView: 3 },
+					}}
+				>
+					{cars.map((car) => (
+						<SwiperSlide key={car.id}>
+							<div className='card-car animate-view'>
+								<img src={car.img} alt="car" />
+								<h1>{car.name}</h1>
+								<p>
+									<CiCalendarDate />
+									<span>{car.year}</span>
+								</p>
+								<p>
+									<IoSpeedometerOutline />
+									<span>{car.velocity}</span>
+								</p>
+								<p>
+									<GoZap />
+									<span>{car.energy}</span>
+								</p>
+								<p>
+									<FiUsers />
+									<span>{car.people}</span>
+								</p>
 
-						<button>
-							Ver Carro
-						</button>
-					</div>
-				))}
+								<button>
+									Ver Carro
+								</button>
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
 			</section>
 
+			{/* Seção de sobre */}
 			<section id='about'>
 				<div className='bg-gradient'>
 					<img src={bg2Img} alt='bg2 car' />
@@ -88,6 +108,7 @@ function App() {
 				</article>
 			</section>
 
+			{/* Seção de contato */}
 			<section id='contact'>
 				<h1>Contato</h1>
 				<p>Entre em contato conosco através de nossos canais de atendimento:</p>
